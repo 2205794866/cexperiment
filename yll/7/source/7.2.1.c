0da05d497023f98e6d500a57f3a28411752bf025
yllhwa <yll2002mail@gamil.com>
2020-12-22 17:04:07 +0800

    add sth

---
+++ yll/7/source/7.2.1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    char u[] = "UVWXYZ";
+    char v[] = "xyz";
+    struct T
+    {
+        int x;
+        char c;
+        char *t;
+    } a[] = {{11, 'A', u}, {100, 'B', v}}, *p = a;
+    //printf("%d", (++p)->x); //100
+    //printf("%c", (p++, p->c)); //B
+    //printf("%c", (*p++->t, *p->t)); //x
+    //printf("%c", *(++p)->t); //x
+    printf("");
+    return 0;
+}
